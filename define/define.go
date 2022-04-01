84211f2293b6c3a4f2ef985cd76499f881f6dfe0
stonejia <stonejia@tencent.com>
2022-04-01 23:13:23 +0800

    update

--- define/define.go
+++ define/define.go
@@ -5,9 +5,15 @@ const (
 	ApiHttpHost = "https://www.okex.com"
 
 	// 账号配置
+	/*
 	ApiKey = "d6cb3787-b0ad-4e18-9231-81e30bac8f01"    // key
 	SecretKey = "B62616B620BB662E255ED4628D74E779"     // secret
 	Passphrase = "Jia119110!"
+	 */
+
+	ApiKey = "d11a2b40-ee5c-455d-93df-7cf577c5789d"    // key
+	SecretKey = "1C32D95C83B8A079CA39EAB161983CC2"     // secret
+	Passphrase = "jia119110@"
 )
 
 
